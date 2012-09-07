aaa878d7f8468c281dcaa93d9e00cb61709234bb
Sai Vemprala <saihemachandra.v@gmail.com>
2012-09-07 15:54:57 -0700

    v0.3
    Support for flash, ADCs, modem init, part of GPS module, etc.

--- Xi202.c
+++ Xi202.c
@@ -10,23 +10,20 @@
 #include <emma/sensors.h>
 #include <emma/flash.h>
 #include <emma/flash_lo.h>
-#include <emma/accelmgr.h>
-#include <emma/motion.h>
 #include <emma/modems/modem.h>
 #include <emma/modems/iridium.h>
-#include <emma/adc.h>
-#include <emma/gps.h>
 #include <emma/watchdog.h>
 #include <emma/rtc_clk.h>
 #include <emma/ports.h>
+
+#define RSTACK 64
+#define CSTACK 512
+
 __flash SENS_INIT Sensors_required[] =
 {
   
   0L
 };
-
-#define RSTACK 64
-#define CSTACK 512
 
 __flash word16 user_rstack_size = RSTACK;
 __flash word16 user_cstack_size = CSTACK;
@@ -37,41 +34,102 @@ word16 user_cstack[CSTACK];
 const u8 ticks_per_sec = 20;
 __flash u16 watchdog_ticks = 100;
 u8 const rtc_holdoff= FALSE;
-u8 mdm_mbox;
-IO_CONF spi_conf;
-IO_HANDLE uh,mdm;
-MODEM_TYPE modemtype = MDM_IRIDIUM;
+u8 mdm_mbox, gps_mbox;
+IO_CONF spi_conf, uart_conf;
+IO_HANDLE io_modem, io_uart;
 MODEM_HANDLE mdm_handle;
- u8 usertask_priority = 1;
-//char buffer[];
+u8 usertask_priority = 1;
+
+void FlashFunctions(int option, u32 address, u8 data, u8 numBytes)
+{
+	u8 id;
+	if(FlashPowerIsOn() == false)
+	{
+		FlashPowerOn();    
+	}
+	FlashInit();
+	switch(option)
+	{
+		case 1:
+		    FlashReadID(id);
+			return id;
+			break;
+		case 2:
+			FlashWriteBytes(address, data, numBytes);
+			break;
+
+		default:
+			break;
+	}
+	FlashPowerOff();
+ }
+
+IO_HANDLE usart_module()
+{
+	usart_config();
+	io_uart = IO_open(IO_UART, &uart_conf);
+	IO_subscribe(io_uart, uart_mbox);
+	IO_start_transaction(io_uart);
+	return uh;
+}
+
+void usart_config()
+{
+	uart_conf.uart_conf.baud = BAUD_57600;
+  	uart_conf.uart_conf.bits = EIGHT_BITS;
+  	uart_conf.uart_conf.parity = PARITY_NONE;
+  	uart_conf.uart_conf.channel = eUART_F0;
+  	uart_conf.uart_conf.blocking = FALSE;
+}
+
 __task void user_task(void)
 {
-  BOOL status;
-  //IO_start_transaction(mdm);
-  //IO_subscribe(mdm, mdm_mbox); 
-  //uh = IO_open(IO_UART, &uart_conf);
-  //mdm = IO_open(IO_UART, &uart_conf);
-  modem_init();
-  mdm_handle = Modem_open(modemtype,mdm);  
-  iridium_init();
-  mdm = (IO_HANDLE)ird_open();
-  ird_config(mdm_handle, &spi_conf);
-  //ird_write(
-  status = Iridium_get_time(mdm_handle);
-  rtc_init();
-  rtc_set_seconds(1000);
-  FlashPowerOn();
-  FlashInit();
-  FlashInit();
-  /*uart_conf.uart_conf.baud = BAUD_57600;
-  uart_conf.uart_conf.bits = EIGHT_BITS;
-  uart_conf.uart_conf.parity = PARITY_NONE;
-  uart_conf.uart_conf.channel = eUART_F0;
-  uart_conf.uart_conf.blocking = FALSE;
-  sprintf(buffer,"hello world");
-  uh = IO_open(IO_UART, &uart_conf);
-  IO_subscribe(uh, uart_mbox);
-  IO_start_transaction(uh);
-  IO_write(uh, (u8 *)buffer, strlen(buffer));*/ 
-  while(1);
+  	while(1)
+	{
+		init_routine();
+	}
 }
+
+void init_routine()
+{
+	gps_module();
+	modem_functions();
+	usart_config();
+	//POLL DATA FROM ADC's
+
+	FlashFunctions(2, xx,xx,xx);
+}
+
+void modem_functions()
+{
+	BOOL mdm_status;
+	int imeibuffer[32];
+	MODEM_PARAMS mdm_params;
+	mdm_params.pm = ON;
+	modem_init();
+	io_modem = IO_open(IO_SPI, &spi_conf);
+	mdm_handle = Modem_open(mdmtype, io_modem);
+	Modem_SetParams(mdm_handle, mdm_params);
+	mdm_status = Modem_get_IMEI((u8 *)imeibuffer);
+}
+
+void gps_module()
+{
+	gps_subscribe(gps_mbox);
+
+	gps_unsubscribe(gps_mbox);
+}
+
+void sensor_module(SENSOR_TYPE sensor_type, u16 interval)
+{
+	sensor_mbox = //ALLOCATE!!
+	H_SENSOR sensorhandle;
+	sensorhandle = sensor_subscribe(sensor_type, MBOX mbox, u16 interval, void *conf);
+	void sensor_unsubscribe(H_SENSOR, MBOX);
+	
+	sensorhandle = sensor_register(SENSOR_TYPE type, SENSOR_FEED feed, MBOX *mbox, u8 task, SENS_CONF_CB config );
+	//-----------TODO---------------
+	void sensor_report_polled(sensor, u8 *data);
+
+}
+
